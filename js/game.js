b6e88f4b4b7baa3eae32df5abf416125875dfb34
Lucas Janing <janinglucas123@gmail.com>
2022-09-03 23:46:33 -0300

    4.0
    add comentários game.js

--- js/game.js
+++ js/game.js
@@ -1,9 +1,11 @@
+// Arquivo game.js onde fica a regra de negócio do jogo.
+
 // Iniciando as variáveis.
-let board = ['', '', '', '', '', '', '', '', ''];
-let playerTime = 0;
-let symbols = ['o', 'x'];
-let gameOver = false;
-let winStates = [
+let board = ['', '', '', '', '', '', '', '', '']; // Quadrados do jogo da velha.
+let playerTime = 0; // Jogador 0 e 1.
+let symbols = ['o', 'x']; // Símbolos dos jogadores, o = 0 e x = 1.
+let gameOver = false; // Iniciando a variável gameOver como falso.
+let winStates = [ // Array de cada quadrado onde será armazenado as posições.
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -14,33 +16,35 @@ let winStates = [
     [2, 4, 6],
 ];
 
+// Função para colocar os símbolos de cada jogador nos quadrados e suas respectivas posições.
 function handleMove(position) {
-    if (gameOver) {
-        return;
+    if (gameOver) { // Se gameOver for verdade. 
+        return; // Retorna e fim de jogo.
     }
-    if (board[position] == "") {
-        board[position] = symbols[playerTime];
+    if (board[position] == "") { // Se o quadrado e sua respectiva posição estiver vazio.
+        board[position] = symbols[playerTime]; // O jogador que estiver na vez escolhe um quadro para jogar.
 
-        gameOver = isWin();
+        gameOver = isWin(); // gameOver recebe função Vitória.
 
-        if (gameOver == false) {
-            playerTime = (playerTime == 0) ? 1 : 0;
+        if (gameOver == false) { // Se gameOver for falso.
+            // Se jogador da vez for zero, troca a vez para o jogador 1 se não jogador 0 joga.
+            playerTime = (playerTime == 0) ? 1 : 0; 
         }
     }
-    return gameOver;
+    return gameOver; // Se for verdadeiro, retorna gameOver.
 }
 
-function isWin() {
-    for (let i = 0; i < winStates.length; i++) {
-        let sequencia = winStates[i];
-        let position1 = sequencia[0];
-        let position2 = sequencia[1];
-        let position3 = sequencia[2];
-        if (board[position1] == board[position2]
-            && board[position1] == board[position3]
-            && board[position1] != "") {
-            return true;
+function isWin() { // Função Vitória
+    for (let i = 0; i < winStates.length; i++) { // Looping dentro do array winStates.
+        let sequencia = winStates[i]; // Variável sequencia recebe cada posição(index) do array winStates.
+        let position1 = sequencia[0]; // Posição 1 recebe sequencia 0.
+        let position2 = sequencia[1]; // Posição 2 recebe sequencia 1.
+        let position3 = sequencia[2]; // Posição 3 recebe sequencia 2.
+        if (board[position1] == board[position2] // Se quadrado da posição 1 for igual quadrado da posição 2.
+            && board[position1] == board[position3] // E quadrado da posição 1 for igual a quadrado da posição 3.
+            && board[position1] != "") { // E quadrado da posição 1 se for vazio.
+            return true; // Retorna verdadeiro.
         }
     }
-    return false;
+    return false; // Retorna falso.
 }
