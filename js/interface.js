10933fcdcaf4942b7b24ba23f3dea8ca1a9b86fe
ljborba <janinglucas123@gmail.com>
2022-09-09 11:08:43 -0300

    add mensagem
    adicionado mensagem de "Fim de jogo"

--- js/interface.js
+++ js/interface.js
@@ -1,9 +1,6 @@
 let playerO = document.getElementById("playerO");
 let playerX = document.getElementById("playerX");
 
-console.log(playerO);
-console.log(playerX);
-
 document.addEventListener("DOMContentLoaded", () => {
     let squares = document.querySelectorAll(".square");
     squares.forEach((square) => {
@@ -15,6 +12,7 @@ function handleClick(event) {
     let square = event.target;
     let position = square.id;
     if (handleMove(position)) {
+        document.getElementById("mensagem").innerHTML = "O jogo acabou!";
         document.getElementById("result").innerHTML = playerTime; // Imprimindo na tela o vencedor da partida.
     };
     updateSquare(position);
@@ -29,11 +27,13 @@ function updateSquare(position) {
 // Botão para reiniciar o jogo.
 let restartButton = document.getElementById("restart");
 restartButton.addEventListener("click", () => { // Quando o usuário clicar no botão dispara o evento click.
+    let mensagem = document.getElementById("mensagem");
     let result = document.getElementById("result");  // Pega o valor do resultado e joga na variável result.
     let squares = document.getElementsByClassName("square"); // Pega todos os quadrados(divs) do jogo e coloca na variável squares.
     for (let i of squares) { // Lopping vai pegar todos os valores do array squares e colocar na variável i.
         i.innerHTML = ""; // Passando um valor vazio para cada posição e imprimindo na tela.
     };
+    mensagem.innerHTML = "";
     result.innerHTML = ""; // Limpa o valor da variável result onde mostra o vencedor.
     board = ['', '', '', '', '', '', '', '', '']; // 
     playerTime = 0;
